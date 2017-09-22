e9e8215a3019e9bbce34420336f207e93262f598
Andre <andre.lom20@outlook.com>
2017-09-21 23:57:19 -0300

    feat(Header): add basic style for the header

--- lolTournament/src/dependency/dependency.ts
+++ lolTournament/src/dependency/dependency.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../app/app.component';
+import { Header } from '../app/components/header/header';
 
 export const declarations = [
   AppComponent,
+  Header,
 ];
 
 export const imports = [
