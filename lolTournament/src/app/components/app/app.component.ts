e9e59bd1d2c9cbca3a4f41bbf5ab322ca03fe3f4
Andre <andre.lom20@outlook.com>
2017-09-23 11:51:20 -0300

    feat(Storage): create localStora helper

--- lolTournament/src/app/components/app/app.component.ts
+++ lolTournament/src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { hasStorage, createStorage } from '../../../helpers/StorageHelper';
 
 @Component({
   selector: 'body',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  /**
+   * start local storage
+   */
+  ngOnInit() {
+    if (!hasStorage()) {
+        createStorage();
+    }
+  }
+
 }
--- lolTournament/src/app/components/home/home.ts
+++ lolTournament/src/app/components/home/home.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { getProperty } from '../../../helpers/StorageHelper';
+import { Tournament } from '../../../models/Tournament';
 
 @Component({
   selector: 'home',
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
-export class Home {
-  tournament:Object = {};
-  
+export class Home implements OnInit {
+  tournament:Tournament;
+
+  ngOnInit() {
+    const tournament = getProperty('tournament');
+    if (tournament) {
+      this.tournament = new Tournament(tournament);
+    }
+  }
+
 }
